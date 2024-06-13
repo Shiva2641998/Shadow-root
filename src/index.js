135b8797961efd80b345c7ff794993e70c9d7e0c
shivam <shiva2641998@gmail.com>
2024-06-14 01:37:12 +0530

    sec 3

--- src/index.js
+++ src/index.js
@@ -10,33 +10,33 @@ import ReactDOM from 'react-dom';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// class ShadowRootContainer extends HTMLElement {
-//   constructor() {
-//       super();
-//       this.attachShadow({ mode: 'open' });
-//   }
+class ShadowRootContainer extends HTMLElement {
+  constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+  }
 
-//   connectedCallback() {
-//       ReactDOM.render(<React.StrictMode>
-//         <Provider store={store}>
-//           <App />
-//         </Provider>
-//       </React.StrictMode>, this.shadowRoot);
-//   }
-// }
+  connectedCallback() {
+      ReactDOM.render(<React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>, this.shadowRoot);
+  }
+}
 
-// customElements.define('shadow-root-container', ShadowRootContainer);
+customElements.define('shadow-root-container', ShadowRootContainer);
 
 // document.body.appendChild(document.createElement('shadow-root-container'));
 
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-   </Provider>
- </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//    </Provider>
+//  </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
